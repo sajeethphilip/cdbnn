0e19ee2445b9a0ae2506292835b5686c68a4c6be
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-03-05 21:09:19 +0530

    Update data_loader.py

--- cdbnn/data_loader.py
+++ cdbnn/data_loader.py
@@ -1,28 +1,39 @@
 import os
-from PIL import Image
+import numpy as np
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
+from .utils import load_image, get_image_properties
 
 class CustomImageDataset(Dataset):
     def __init__(self, img_dir, transform=None):
         self.img_dir = img_dir
         self.transform = transform
-        self.img_labels = os.listdir(img_dir)
+        self.img_files = os.listdir(img_dir)
+        self.image_properties = self._get_image_properties()
+
+    def _get_image_properties(self):
+        """Get the properties of the first image in the directory."""
+        first_image_path = os.path.join(self.img_dir, self.img_files[0])
+        first_image = load_image(first_image_path)
+        return get_image_properties(first_image)
 
     def __len__(self):
-        return len(self.img_labels)
+        return len(self.img_files)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels[idx])
-        image = Image.open(img_path).convert('L')  # Convert to grayscale
-        label = int(self.img_labels[idx].split('_')[0])  # Assuming filename format: label_imageid.ext
+        img_path = os.path.join(self.img_dir, self.img_files[idx])
+        image = load_image(img_path)
+
+        # Convert grayscale images to 3 channels if needed
+        if len(image.shape) == 2:
+            image = np.stack([image] * 3, axis=-1)
+
+        # Convert to PIL Image for transformations
+        image = Image.fromarray(image)
+
         if self.transform:
             image = self.transform(image)
-        return image, label
 
-def get_transform():
-    return transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5], std=[0.5])
-    ])
+        # Assuming filename format: label_imageid.ext
+        label = int(self.img_files[idx].split('_')[0])
+        return image, label
