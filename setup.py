573b22bbb5f1b42cfe3555e99b178585578d5110
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-03-05 19:07:30 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cdbnn",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "torchvision",
+        "pydicom",
+        "astropy",
+        "Pillow",
+        "pandas"
+    ],
+    entry_points={
+        "console_scripts": [
+            "mnist_cnn=main:main"
+        ]
+    }
+)
