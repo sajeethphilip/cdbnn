6fabbe36ca0ed848cc8358d7a0cf7cb9ac0f562d
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-03-05 21:46:14 +0530

    Update data_loader.py

--- cdbnn/data_loader.py
+++ cdbnn/data_loader.py
@@ -2,18 +2,29 @@ import os
 import numpy as np
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
-from .utils import load_image, get_image_properties
+from .utils import load_image, get_image_properties, prepare_dataset, infer_classes_from_folder
 
 class CustomImageDataset(Dataset):
     def __init__(self, img_dir, transform=None):
         self.img_dir = img_dir
         self.transform = transform
-        self.img_files = os.listdir(img_dir)
+        self.classes = infer_classes_from_folder(img_dir)
+        self.class_to_idx = {cls: i for i, cls in enumerate(self.classes)}
+        self.img_files = self._get_image_files()
         self.image_properties = self._get_image_properties()
 
+    def _get_image_files(self):
+        """Get a list of all image files in the dataset."""
+        img_files = []
+        for cls in self.classes:
+            cls_dir = os.path.join(self.img_dir, cls)
+            for file in os.listdir(cls_dir):
+                img_files.append((os.path.join(cls_dir, file), self.class_to_idx[cls]))
+        return img_files
+
     def _get_image_properties(self):
-        """Get the properties of the first image in the directory."""
-        first_image_path = os.path.join(self.img_dir, self.img_files[0])
+        """Get the properties of the first image in the dataset."""
+        first_image_path = self.img_files[0][0]
         first_image = load_image(first_image_path)
         return get_image_properties(first_image)
 
@@ -21,7 +32,7 @@ class CustomImageDataset(Dataset):
         return len(self.img_files)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_files[idx])
+        img_path, label = self.img_files[idx]
         image = load_image(img_path)
 
         # Convert grayscale images to 3 channels if needed
@@ -34,6 +45,4 @@ class CustomImageDataset(Dataset):
         if self.transform:
             image = self.transform(image)
 
-        # Assuming filename format: label_imageid.ext
-        label = int(self.img_files[idx].split('_')[0])
         return image, label
